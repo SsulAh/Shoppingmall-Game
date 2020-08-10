50d932a07a170f198d486dcf3b60cb2deee187cb
BingBing <hwangwonbins@gmail.com>
2020-08-11 01:48:12 +0900

    javascript coding (main.js)
    data.json 호출 / 구현

---
+++ src/main.js
@@ -0,0 +1,49 @@
+
+//Fetch the items from the JSON file
+function loadItems() {
+    return fetch('data/data.json')
+    .then(response => response.json())
+    .then(json => json.items);
+}
+
+//Update the list with the given items
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTML5String(item));
+}
+
+// Create HTML list item from the given data item
+function createHTML5String(item) {
+    return  `
+    <li class = "item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
+function onButtonClick (event, items) {
+        const datset = evnet.target.dataset;
+        const key = dataset.key;
+        const value = dataset.value;
+
+        if (key === null || value == null) {
+            return;
+        }
+        const filtered = items.filter(item => item[key] === value);
+        console.log(filtered);
+        displayItems(items.filter)
+}
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+//main
+loadItems()
+    .then(items => {
+        displayItems(items);
+    })
+.catch(console.log)
